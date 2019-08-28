5d939db81e5cf2090becc7b7f1cc177ea09b2d9d
Paul Oladele <paul.oladele@gamesys.co.uk>
2019-08-28 16:24:04 +0100

    jwt filters

---
+++ src/main/java/com/devconnection/Gateway/config/JwtAuthenticationFilter.java
@@ -0,0 +1,44 @@
+package com.devconnection.Gateway.config;
+
+import java.io.IOException;
+import java.util.Collections;
+
+import javax.servlet.FilterChain;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.devconnection.Gateway.messages.LoginMessage;
+import com.devconnection.Gateway.services.AuthenticationService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    public JwtAuthenticationFilter(String defaultFilterProcessesUrl, AuthenticationManager authenticationManager) {
+        setAuthenticationManager(authenticationManager);
+        setFilterProcessesUrl(defaultFilterProcessesUrl);
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws AuthenticationException {
+        try {
+            LoginMessage creds = new ObjectMapper().readValue(httpServletRequest.getInputStream(), LoginMessage.class);
+            return getAuthenticationManager().authenticate(
+                    new UsernamePasswordAuthenticationToken(creds.getUsername(), creds.getPassword(), Collections.emptyList()
+                    )
+            );
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) {
+        AuthenticationService.addToken(response, authResult);
+    }
+}
---
+++ src/main/java/com/devconnection/Gateway/config/JwtAuthorizationFilter.java
@@ -0,0 +1,31 @@
+package com.devconnection.Gateway.config;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import com.devconnection.Gateway.services.AuthenticationService;
+
+public class JwtAuthorizationFilter extends BasicAuthenticationFilter {
+
+    public JwtAuthorizationFilter(AuthenticationManager authenticationManager) {
+        super(authenticationManager);
+    }
+
+    @Override
+    public void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+        Authentication authentication = AuthenticationService.getAuthentication(request);
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        filterChain.doFilter(request, response);
+    }
+}
