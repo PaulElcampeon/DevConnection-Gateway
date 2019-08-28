83ce74784291bc2c86e18ec5ea6770f804d6857f
Paul Oladele <paul.oladele@gamesys.co.uk>
2019-08-28 16:23:26 +0100

    domain objects

---
+++ src/main/java/com/devconnection/Gateway/domain/User.java
@@ -0,0 +1,29 @@
+package com.devconnection.Gateway.domain;
+
+import lombok.Data;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import com.devconnection.Gateway.messages.RegistrationMessage;
+
+@Data
+@Document(collection = "USERS")
+public class User {
+
+    @Id
+    private String username;
+    private String password;
+    private List<String> roles = Arrays.asList("USER");
+
+    public User() {
+    }
+
+    public User(RegistrationMessage registrationMessage) {
+        this.username = registrationMessage.getUsername();
+        this.password = registrationMessage.getPassword();
+    }
+}
