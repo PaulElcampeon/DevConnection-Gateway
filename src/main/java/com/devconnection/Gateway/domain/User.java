78fe4ee72fefb642355599cf3a51e5b516b82b05
Paul Oladele <paul.oladele@gamesys.co.uk>
2019-08-28 16:59:08 +0100

    modified

--- src/main/java/com/devconnection/Gateway/domain/User.java
+++ src/main/java/com/devconnection/Gateway/domain/User.java
@@ -15,6 +15,7 @@ import com.devconnection.Gateway.messages.RegistrationMessage;
 public class User {
 
     @Id
+    private String email;
     private String username;
     private String password;
     private List<String> roles = Arrays.asList("USER");
