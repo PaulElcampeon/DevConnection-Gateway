7aabe7581aebcb6431d04355793e75bf903f92f3
Paul Oladele <paul.oladele@gamesys.co.uk>
2019-08-30 14:49:11 +0100

    removed unused import

--- src/main/java/com/devconnection/Gateway/config/SecurityConfig.java
+++ src/main/java/com/devconnection/Gateway/config/SecurityConfig.java
@@ -3,7 +3,6 @@ package com.devconnection.Gateway.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
